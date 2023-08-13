d4c7831dea85b4cd20c8f46b969b7b820d206fd1
harshit <harshit.sahu@gmail.com>
2023-08-14 00:42:33 +0530

    exactspace_harshit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors");
+const path = require("path");
+const app = express()
+app.use(cors());
+
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend.html"));
+});
+
+app.post("/",(req,res)=>{
+  console.log(req.body)
+    if (req.is('json') && Object.keys(req.body).length != 0) {
+        res.send(true);
+      } else {
+        res.send(false);
+      }
+})
+
+
+app.listen(3000,()=>{
+    console.log("server up")
+})
